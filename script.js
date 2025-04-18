9044088d8241609f741509a03febdaf441f14741
Felipe <118012333+FelipePaterni@users.noreply.github.com>
2025-04-18 17:21:12 -0300

    closes # 4 - add calculator operations for addition, subtraction, multiplication, and division

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-function calc(){
+function calc(char){
    var num1 = parseInt(document.getElementById("num1").value);
    var num2 = parseInt(document.getElementById("num2").value);
-
-   var output = num1+num2;
+   var output;
+switch (char) {
+    case '+':
+        output = num1 + num2;
+        break;
+    case '-':
+        output = num1 - num2;
+        break;
+    case '*':
+        output = num1 * num2;
+        break;
+    case '/':
+        output = num1 / num2 ;
+        break;
+    
+}
+  
 
   document.getElementById("output").value = output;
 }
