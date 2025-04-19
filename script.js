b329423aa84db7872410e8683ffdfb40f1495f93
Felipe <118012333+FelipePaterni@users.noreply.github.com>
2025-04-19 12:56:20 -0300

    closes #5,#6 - add calculator layout enhancements with title and history display

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
-function calc(char){
-   var num1 = parseInt(document.getElementById("num1").value);
-   var num2 = parseInt(document.getElementById("num2").value);
-   var output;
-switch (char) {
-    case '+':
-        output = num1 + num2;
-        break;
-    case '-':
-        output = num1 - num2;
-        break;
-    case '*':
-        output = num1 * num2;
-        break;
-    case '/':
-        output = num1 / num2 ;
-        break;
-    
-}
-  
-
+function calc(char) {
+  var num1 = parseInt(document.getElementById("num1").value);
+  var num2 = parseInt(document.getElementById("num2").value);
+  var output;
+  switch (char) {
+    case "+":
+      output = num1 + num2;
+      break;
+    case "-":
+      output = num1 - num2;
+      break;
+    case "*":
+      output = num1 * num2;
+      break;
+    case "/":
+      output = num1 / num2;
+      break;
+  }
   document.getElementById("output").value = output;
+
+  var newHistory =  "<p>" + num1 + " " + char + " " + num2 + " = " + output + "</p>";
+ 
+  var history = document.getElementById("history");
+history.innerHTML = newHistory + history.innerHTML;
+
+  if (history.children.length > 10) {
+    history.removeChild(history.childNodes[10]);
+  }
 }
